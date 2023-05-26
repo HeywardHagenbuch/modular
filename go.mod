fd22095926ad5d014c4bb5481f326992ab96282d
Heyward Hagenbuch <Heyward360@gmail.com>
2023-05-26 12:38:04 -0400

    Playing with tags

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/HeywardHagenbuch/modular
+
+go 1.20
+
+require github.com/HeywardHagenbuch/puppy v1.3.0
+
+require github.com/HeywardHagenbuch/dog v0.0.0-20230525210513-8cac314d805f // indirect
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/HeywardHagenbuch/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+	puppy.From13()
+	puppy.From11()
+	puppy.From12()
 }
